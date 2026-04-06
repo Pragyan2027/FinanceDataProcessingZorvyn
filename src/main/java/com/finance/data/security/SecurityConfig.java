055eaeb7305897cd2c913cae43f446c9906909d1
Pragyan2027 <senpragyan3916@gmail.com>
2026-04-06 18:16:20 +0530

    New updates

--- src/main/java/com/finance/data/security/SecurityConfig.java
+++ src/main/java/com/finance/data/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/users").permitAll()
+                        .requestMatchers("/", "/auth/**", "/users").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
